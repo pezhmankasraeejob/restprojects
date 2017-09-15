4edffd5f389519c5ecde21e1c708582c1cb4a55a
Pezhman Kasraee <pezhman.kasraee@sigmatechnology.se>
2017-09-15 14:45:58 +0200

    FEAT ADD: returns student object as a json file

--- university/src/main/java/se/sigmatechnology/controller/UniversityController.java
+++ university/src/main/java/se/sigmatechnology/controller/UniversityController.java
@@ -1,7 +1,9 @@
 package se.sigmatechnology.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.sigmatechnology.entities.Student;
 
 /**
  * Created by pke on 2017-09-15.
@@ -20,5 +22,10 @@ public class UniversityController {
                 + "Application Description: This is a Application to manage students and their course\n"
                 + "Technology: JDK 1.8 and RESTful architecture.";
         return result;
+    }
+
+    @RequestMapping("/student/{id}:{firstname}:{surname}")
+    public Student getStudent(@PathVariable("id") String id, @PathVariable("firstname") String firstname, @PathVariable("surname") String surname){
+        return new Student(id, firstname, surname);
     }
 }
